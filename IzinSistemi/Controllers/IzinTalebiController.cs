fb134ba03af7791fbdbe70f4cc62ba468c2f6c54
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2024-07-23 10:04:38 +0300

    Son tasarım ve düzenleme

--- IzinSistemi/Controllers/IzinTalebiController.cs
+++ IzinSistemi/Controllers/IzinTalebiController.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.UI;
 
@@ -20,6 +23,8 @@ namespace IzinSistemi.Controllers
             return View();
         }
 
+        
+
         [HttpGet]
      
         public ActionResult Talep()
@@ -31,6 +36,7 @@ namespace IzinSistemi.Controllers
             return View();
         }
         [HttpPost]
+        
         public ActionResult Talep(IzinTalebi model)
         {
             var kullanici = (string)Session["Mail"];
@@ -46,8 +52,11 @@ namespace IzinSistemi.Controllers
                     IzinTalepPersoneId = uye.PId,  // İlgili personelin ID'si
                     Baslangic = model.Baslangic,
                     Bitis = model.Bitis,
+                    Gun = model.Gun,
                    
                 };
+
+
 
                 db.IzinTalebi.Add(yeniIzin);
                 db.SaveChanges();
@@ -55,7 +64,8 @@ namespace IzinSistemi.Controllers
 
             return RedirectToAction("Index","Panel");
         }
-     
+
+        [Authorize(Roles ="B")]
         public ActionResult TalepGetir()
         {
 
@@ -66,28 +76,42 @@ namespace IzinSistemi.Controllers
        
         public ActionResult TalepOnay1(IzinTalebi P )
         {
+
+            var personeltut = db.Personel;
+
+            MailController newMail = new MailController();
             var gun = P.Gun;
             var id = P.Id;
-            var deger = db.IzinTalebi.FirstOrDefault(i => i.Id ==P.Id);
-          
+            var deger = db.IzinTalebi.FirstOrDefault(i => i.Id == P.Id);
+            
+
+            var personel1 = deger.Gun;
+            var kullanici = (string)Session["Mail"];
+            var uye = db.Personel.FirstOrDefault(x => x.Mail == kullanici);
+
+
             var personel = deger.IzinTalepPersoneId;
+            var tut = deger.Personel.Mail;
+            var personell = db.Personel.Find(personel);
+            
             var deger2 = db.Personel.Find(personel);
 
+        
+            
             var izinmiktarı = P.Gun;
             
             if(deger != null)
             {
                 deger.Onay = true;
-                deger2.KalanIzin = deger2.KalanIzin - P.Gun;
+                deger2.KalanIzin = deger2.KalanIzin - personel1;
+                deger2.KullanılanIzin = deger2.KullanılanIzin + personel1;
+                newMail.Index(P, tut);
                 db.SaveChanges();
             }
-
+         
             var deger1 = db.IzinTalebi.ToList();
             return RedirectToAction("TalepGetir","IzinTalebi");
         }
-
-
-    
         public ActionResult TalepSil(IzinTalebi P)
         {
             var id = P.Id;
@@ -98,7 +122,39 @@ namespace IzinSistemi.Controllers
             return RedirectToAction("Index","Panel");
         }
 
-     
+
+        //[HttpPost]
+        //public ActionResult Mail(IzinTalebi model)
+        //{
+        //    MailMessage mailim = new MailMessage();
+        //    mailim.To.Add("ibrahimtulumcu@hotmail.com");
+        //    mailim.From = new MailAddress("ibrahimtulumcu@hotmail.com");
+        //    mailim.Subject = "Bize Ulaşın Sayfasından Mesajınız Var. " + model.Baslangic;
+        //    mailim.Body = "Sayın yetkili, " + model.Bitis;
+        //    mailim.IsBodyHtml = true;
+
+
+        //    SmtpClient smtp = new SmtpClient();
+        //    smtp.Credentials = new NetworkCredential("ibrahimtulumcu@hotmail.com", "chtd iids rorl wzpw");
+        //    smtp.Port = 587;
+        //    smtp.Host = "smtp.hotmail.com";
+        //    smtp.EnableSsl = true;
+
+        //    try
+        //    {
+        //        smtp.Send(mailim);
+        //        TempData["Message"] = "Mesajınız iletilmiştir. En kısa zamanda size geri dönüş sağlanacaktır.";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["Message"] = "Mesaj gönderilemedi.Hata nedeni:" + ex.Message;
+        //    }
+
+        //    return View();
+
+        //}
+
+
 
     }
 
---
+++ IzinSistemi/Controllers/MailController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+using IzinSistemi.Models.Entity;
+
+namespace IzinSistemi.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IzinTalebi model, string tut)
+        {
+            MailMessage mailim = new MailMessage();
+            mailim.To.Add(tut);
+            mailim.From = new MailAddress("ibrahimtulumcu@hotmail.com");
+            mailim.Subject = "Izin Talebi Bilgilendirme. " + model.IzinTip;
+            mailim.Body = "İzin talebiniz Onaylanmıştır " + model.Baslangic + "-" + model.Bitis;
+            mailim.IsBodyHtml = true;
+
+ 
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("ibrahimtulumcu@hotmail.com", "muslera.58");
+            smtp.Port = 587;
+            smtp.Host = "smtp-mail.outlook.com";
+            smtp.EnableSsl = true;
+
+            try
+            {
+                smtp.Send(mailim);
+                TempData["Message"] = "Mesajınız iletilmiştir. En kısa zamanda size geri dönüş sağlanacaktır.";
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = "Mesaj gönderilemedi.Hata nedeni:" + ex.Message;
+            }
+
+            return View();
+
+        }
+
+    }
+}
